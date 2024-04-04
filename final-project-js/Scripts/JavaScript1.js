0e0b81d00cbc3e41ac7621e6988471b6333f071f
N <166038881+naama-80820@users.noreply.github.com>
2024-04-04 19:34:57 +0300

    Bombing-planes

---
+++ final-project-js/Scripts/JavaScript1.js
@@ -0,0 +1,137 @@
+﻿var numPic = 1;
+var selName;
+var playerName;
+
+
+function Game(playerName, numElements, level, numPic) {
+    this.gamePlayerName = playerName;
+    this.gameNumElements = numElements;
+    this.gameLevel = level;
+    this.gameNumPic = numPic;
+}
+
+
+function Champion(CName, CPoints) {
+    this.championName = CName;
+    this.championPoints = CPoints;
+}
+
+
+function DuplicateNames(currentName) {
+
+    var duplicate = false;
+
+    var theOption = document.querySelectorAll("#selectName>option");
+
+    for (var i = 0; i < theOption.length; i++) {
+        if (theOption[i].text == currentName) {
+            duplicate = true;
+        }
+    }
+    return duplicate;
+}
+
+function putNames() {
+    selName = document.querySelector("#selectName");
+
+    for (var level = 1; level <= 3; level++) {
+        var key = "Level" + level;
+        var Champions = JSON.parse(localStorage.getItem(key));
+        if (Champions != null) {
+            for (var i = 0; i < Champions.length; i++) {
+
+                Duplicate = DuplicateNames(Champions[i].championName);
+
+                if (Duplicate == false) {
+                    var theOption = document.createElement("option");
+                    theOption.textContent = Champions[i].championName;
+                    selName.appendChild(theOption);
+
+                }
+            }
+
+            document.querySelector("#PlayerName").style.display = "none"
+        }
+    }
+}
+
+function showText() {
+
+    document.querySelector("#PlayerName").style.display = "inline";
+
+    selName.selectedIndex = 0;
+}
+
+function HideText() {
+
+    document.querySelector("#PlayerName").style.display = "none";
+}
+
+function RemoveImages() {
+
+    numPic = 1;
+    var ArrImg = document.querySelectorAll(".imgElements");
+    var parentImg = document.querySelector("#ParentImages");
+    for (var i = 0; i < ArrImg.length; i++) {
+        parentImg.removeChild(ArrImg[i]);
+    }
+}
+
+function CreateImages() {
+
+    var parentImg = document.querySelector("#ParentImages");
+    var ArrImg = [];
+    for (var i = 0; i < 6; i++) {
+        ArrImg[i] = document.createElement("img");
+        parentImg.appendChild(ArrImg[i]);
+        ArrImg[i].src = "../img/" + i + ".JPG";
+        ArrImg[i].setAttribute("class", "imgElements");
+        ArrImg[i].onclick = function (e) {
+            chooseImg(e.target);
+        }
+    }
+    ArrImg[0].style.border = "2px dotted #000000"
+    numPic = 0;
+}
+
+function chooseImg(obj) {
+
+    var ArrImg = document.querySelectorAll(".imgElements");
+    for (var i = 0; i < ArrImg.length; i++) {
+        if (ArrImg[i] == obj) {
+            ArrImg[i].style.border = "2px dotted #000000";
+            numPic = i;
+        }
+        else {
+
+            ArrImg[i].style.border = "none";
+        }
+    }
+}
+
+function goNext() {
+
+    selName = document.querySelector("#selectName");
+    if (selName.selectedIndex == 0) {
+
+        playerName = document.querySelector("#PlayerName").value;
+    }
+    else {
+
+        playerName = selName.options[selName.selectedIndex].textContent;
+    }
+
+
+    var numElements = document.querySelector("#NumElements").value;
+
+    var level = document.querySelector("#level").value;
+
+
+    var objGame = new Game(playerName, numElements, level, numPic);
+
+    sessionStorage.Game = JSON.stringify(objGame);
+
+    window.location = "HtmlPage2.html";
+}
+
+
---
+++ final-project-js/Scripts/JavaScript2.js
@@ -0,0 +1,196 @@
+﻿//page2
+
+var WinWidth, WinHeight;
+var numOfImages = 0;
+var Mouse;
+var points;
+var MyImg = [];
+var Dir1 = [], Dir2 = [];
+var ImgSize = [];
+var TheLeft = [], TheTop = [];
+var objGame;
+
+function Player(PName, PPoints, PLevel) {
+    this.playerName = PName;
+    this.playerPoints = PPoints;
+    this.playerLevel = PLevel;
+}
+
+
+
+function putDetails() {
+
+    objGame = JSON.parse(sessionStorage.getItem('Game'));
+
+
+
+    document.querySelector("#PlayerName").textContent = objGame.gamePlayerName;
+
+    var numElements = objGame.gameNumElements;
+
+    var numPic = objGame.gameNumPic;
+    if (numPic == -1) {
+
+        numPic = Math.floor(Math.random() * 3);
+    }
+    var TheElement = "../img/" + numPic + ".jpg";
+
+    var points = Math.floor(Math.random() * 100) + 10;
+
+}
+
+function Game(playerName, numElements, level, numPic) {
+    this.gamePlayerName = playerName;
+    this.gameNumElements = numElements;
+    this.gameLevel = level;
+    this.gameNumPic = numPic;
+}
+
+function addImg() {
+    var endPic = document.createElement("img");
+    endPic.setAttribute("src", "../img/end_2.jpg");
+    document.querySelector("#finish").appendChild("endPic");
+    document.body.style.backgroundImage = "url('../img/end_2.jpg')";
+}
+
+
+
+function changeColors() {
+    var blockElements = document.getElementsByClassName("block");
+    var buttonElements = document.getElementsByClassName("button");
+    //var score = document.getElementById("score");
+    //score.style.backgroundColor = "#c9370b";
+    for (var i = 0; i < blockElements.length; i++) {
+        blockElements[i].style.backgroundColor = "#c9370b";
+    }
+
+    for (var j = 0; j < buttonElements.length; j++) {
+        buttonElements[j].style.backgroundColor = "#c9370b";
+    }
+}
+
+function Init() {
+
+    // fun for inited where the page is inlaoud
+    WinWidth = window.innerWidth;
+    WinHeight = window.innerHeight;
+
+    points = 0;
+
+    putDetails();
+    document.querySelector("#PlayerName").textContent = objGame.gamePlayerName;
+    document.querySelector("#Points").textContent = points;
+
+    var numPic = objGame.gameNumPic;
+    if (numPic == -1) {
+
+        numPic = Math.floor(Math.random() * 10);
+    }
+
+
+    var inter = setInterval(function () {
+        WinWidth = window.innerWidth;
+        WinHeight = window.innerHeight;
+
+        numOfImages++;
+
+        MyImg[numOfImages] = document.createElement("img");
+
+        var TheElement = "../img/" + numPic + ".jpg";
+
+        MyImg[numOfImages].src = TheElement;
+
+        document.body.appendChild(MyImg[numOfImages]);
+
+        ImgSize[numOfImages] = Math.floor(Math.random() * 80) + 50;
+        MyImg[numOfImages].style.width = ImgSize[numOfImages] + "px";
+        MyImg[numOfImages].style.height = ImgSize[numOfImages] + "px";
+        MyImg[numOfImages].style.position = "absolute";
+
+        TheLeft[numOfImages] = Math.floor(Math.random() * (WinWidth - ImgSize[numOfImages]));
+        MyImg[numOfImages].style.left = TheLeft[numOfImages] + "px";
+
+        TheTop[numOfImages] = Math.floor(Math.random() * (WinHeight - ImgSize[numOfImages]));
+        MyImg[numOfImages].style.top = TheTop[numOfImages] + "px";
+
+        MyImg[numOfImages].classList.add("NewImages");
+
+        Dir1[numOfImages] = Math.floor(Math.random() * 2);
+        if (Dir1[numOfImages] == 0)
+            Dir1[numOfImages] = -1;
+        Dir2[numOfImages] = Math.floor(Math.random() * 2);
+        if (Dir2[numOfImages] == 0)
+            Dir2[numOfImages] = -1;
+
+        MyImg[numOfImages - 1].onclick = function (e) {
+            document.body.removeChild(e.target);
+            points+=10;
+            document.querySelector("#Points").textContent = points;
+
+        }
+
+
+
+
+        var numElements = objGame.gameNumElements;
+        if (numOfImages >= numElements) {
+            clearInterval(inter);// stopping the clock
+            var TheImages = document.querySelectorAll("img");
+            for (var i = 0; i < TheImages.length; i++) {
+                TheImages[i].onclick = null;
+
+                document.body.removeChild(TheImages[i]);
+                document.querySelector("#finish").textContent = "The Game Is Over";
+                document.body.style.backgroundImage = "url('../img/end.jpg')";
+                changeColors();
+            }
+        }
+    }, 500);
+
+
+    var interMove = setInterval(function () {
+        for (var i = 1; i < numOfImages; i++) {
+
+
+            TheLeft[i] = parseInt(MyImg[i].style.left, 10);
+            TheLeft[i] += 3 * Dir1[i];
+
+            MyImg[i].style.left = TheLeft[i] + "px";
+            if ((TheLeft[i] <= 0) || (TheLeft[i] + ImgSize[i] + 10 >= WinWidth + 3))
+                Dir1[i] = -Dir1[i];
+
+
+            TheTop[i] = parseInt(MyImg[i].style.top, 10);
+            TheTop[i] += 3 * Dir2[i];
+
+            MyImg[i].style.top = TheTop[i] + "px";
+            if ((TheTop[i] <= 0) || (TheTop[i] + ImgSize[i] >= WinHeight + 3))
+                Dir2[i] = -Dir2[i];
+
+        }
+    }, 50);
+
+}
+
+function goChampions() {
+    var playerName = document.querySelector("#PlayerName").textContent;
+    // num points
+    var points = document.querySelector("#Points").textContent;
+    //level
+    var level = objGame.gameLevel;
+    var objPlayer = new Player(playerName, points, level);
+    sessionStorage.Player = JSON.stringify(objPlayer);
+
+    window.location = "thirdPage.html";
+}
+
+
+function P2toP3() {
+    window.location = "HtmlPage3.html";
+}
+
+function P2toP1() {
+    window.location = "HtmlPage1.html";
+
+}
+
